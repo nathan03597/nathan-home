6197bcdf0bab27644ddec3c779a826d83ec6dfd0
Nathan Ma <nathan@intalio.com>
2012-09-26 17:41:20 +0800

    file upload

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-
+var util = require('util');
 /*
  * GET home page.
  */
+var data = {
+    title: "Nathan Home",
+    show_name: "Nathan Ma",
+    contact_mail: "nathan.mading@gmail.com"
+};
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render("index", data);
 };
